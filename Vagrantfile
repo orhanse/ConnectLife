5d961c6e802ec96cd6a362c1fe99b32c0da2b632
H. Turgut Uyar <uyar@itu.edu.tr>
2015-07-24 17:26:30 +0300

    added postgresql connection example

--- Vagrantfile
+++ Vagrantfile
@@ -6,6 +6,7 @@ Vagrant.configure("2") do |config|
     config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-i386-vagrant-disk1.box"
 
     config.vm.network :forwarded_port, guest: 5432, host: 5432
+    config.vm.network :forwarded_port, guest: 5000, host: 5001
 
     config.vm.provider "virtualbox" do |vb|
         #vb.customize ["modifyvm", :id, "--memory", "1024"]
---
+++ server_pg.py
@@ -0,0 +1,56 @@
+from flask import Flask
+from flask import render_template
+
+from datetime import datetime
+import json
+import os
+import psycopg2 as dbapi2
+from random import randrange
+import re
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    now = datetime.now()
+    return render_template('home.html', current_time=now.ctime())
+
+
+@app.route('/random')
+def random_numbers():
+    with dbapi2.connect(app.config['db_params']) as db:
+        with db.cursor() as cursor:
+            cursor.execute("CREATE TABLE IF NOT EXISTS dummy (num INTEGER PRIMARY KEY)")
+            cursor.execute("INSERT INTO dummy (num) VALUES (%s)", (randrange(200),))
+            db.commit()
+
+            cursor.execute("SELECT num FROM dummy")
+            nums = [r[0] for r in cursor.fetchall()]
+    return "Stored numbers: %s" % ', '.join(map(str, nums))
+
+
+def get_db_params():
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        parsed = json.loads(VCAP_SERVICES)
+        uri = parsed["elephantsql"][0]["credentials"]["uri"]
+        match = re.match('postgres://(.*?):(.*?)@(.*?):5432/(.*)', uri)
+        user = match.group(1)
+        password = match.group(2)
+        host = match.group(3)
+        dbname = match.group(4)
+    else:
+        host = 'localhost'
+        user = 'vagrant'
+        password = 'vagrant'
+        dbname = 'itucsdb'
+    return "dbname='%s' user='%s' host='%s' password='%s'" % (dbname, user, host, password)
+
+
+if __name__ == '__main__':
+    app.config['db_params'] = get_db_params()
+    PORT = int(os.getenv('VCAP_APP_PORT', '5000'))
+    app.run(host='0.0.0.0', port=int(PORT))
