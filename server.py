856ebdb73deb084967bff88ae0910430613deb93
orhanse <orhanse@itu.edu.tr>
2016-10-28 11:36:15 +0300

    Added elephant SQL Added a simple counter

--- server.py
+++ server.py
@@ -9,20 +9,53 @@ from flask import render_template
 from flask import redirect
 from flask import request
 from flask.helpers import url_for
-#from config import app
 
 
 app = Flask(__name__)
 
-
-
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
 
 @app.route('/')
 def home_page():
     now = datetime.datetime.now()
     return render_template('home.html', current_time=now.ctime())
 
+@app.route('/initdb')
+def initialize_database():
+    connection = dbapi2.connect(app.config['dsn'])
+    cursor = connection.cursor()
 
+    query = """DROP TABLE IF EXISTS COUNTER"""
+    cursor.execute(query)
+    query = """CREATE TABLE COUNTER (N INTEGER)"""
+    cursor.execute(query)
+    query = """INSERT INTO COUNTER(N) VALUES(0)"""
+    cursor.execute(query)
+
+    connection.commit()
+    return redirect(url_for('home_page'))
+
+@app.route('/count')
+def counter_page():
+    connection = dbapi2.connect(app.config['dsn'])
+    cursor = connection.cursor()
+
+    query = "UPDATE COUNTER SET N = N + 1"
+    cursor.execute(query)
+    connection.commit()
+
+    query = "SELECT N FROM COUNTER"
+    cursor.execute(query)
+    count = cursor.fetchone()[0]
+    return "This page was accesed %d times." % count
 
 @app.route('/kisiler')
 def kisiler_sayfasi():
@@ -58,4 +91,10 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='vagrant' password='vagrant'
+                               host='localhost' port=54321 dbname='itucsdb'"""
     app.run(host='0.0.0.0', port=port, debug=debug)
